5d4ba822f28f0bc1bf67456ccd79c82535fb71ba
iuanshuai <ishuaiyuan@gmail.com>
2021-03-25 17:20:43 -0400

    Add env

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,25 @@ import React from 'react';
 import './App.css';
 import { AppBar, Button } from '@material-ui/core';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
+// import { makeStyles } from '@material-ui/core/styles';
 
 function App() {
+  // const classes = useStyles();
   const matches = useMediaQuery('(min-width:600px)');
+
   return (
     <div className="">
       <AppBar position="static">
         {matches ? (
-          <Button>B</Button>
+          <div>
+            <Button color="inherit">Home</Button>
+            <Button color="inherit">Demo Page</Button>
+          </div>
         ) : (
           <div>
-            <Button>Hellp</Button>
-            <Button>world</Button>
-            <Button>sure</Button>
+            <Button color="inherit">Home</Button>
+            <Button color="inherit">Demo Page</Button>
+            <Button color="inherit">Sign in</Button>
           </div>
         )}
       </AppBar>
