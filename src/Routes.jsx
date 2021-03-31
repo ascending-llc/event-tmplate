7d79a7367b575b67b08933f4eb0bfeda2827fc4c
iuanshuai <ishuaiyuan@gmail.com>
2021-03-31 15:31:39 -0400

    Update route

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import {
   Switch, Route, useLocation,
 } from 'react-router-dom';
+import Home from './pages/Home';
 
 export default function Routes() {
   const location = useLocation();
   return (
     <Switch>
-      <Route exact path="/" />
+      <Route exact path="/" component={Home} />
     </Switch>
   );
 }
